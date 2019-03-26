97b1c6b75d5918c47ccd1263ce32b776692e1bf2
Yogesh Mangnaik <yogeshmangnaik2012@gmail.com>
2019-03-26 23:07:15 +0530

    Python Code to create hash

--- Certificates.sol
+++ Certificates.sol
@@ -1,21 +1,53 @@
 pragma solidity >=0.4.22 <0.6.0;
 
+import "./MyToken.sol";
+
 contract Certificates{
     
     mapping (uint => bytes32) public certificates;
     address owner;
+    MyToken public tokenContract;
+    uint256 verificationValue = 30;
+    uint decimals = 18;
     
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
     
-    constructor() public{
+        //function to verify the merkle root generated with hash of leaf
+    function verifyProof(bytes32[] memory proof, 
+        bytes32 root, 
+        bytes32 leaf) internal pure returns (bytes32) {
+        bytes32 computedHash = leaf;
+
+        for (uint256 i = 0; i < proof.length; i++) {
+            bytes32 proofElement = proof[i];
+
+            if (computedHash < proofElement) {
+                // Hash(current computed hash + current element of the proof)
+                computedHash = keccak256(abi.encodePacked(computedHash, proofElement));
+            } else {
+                // Hash(current element of the proof + current computed hash)
+                computedHash = keccak256(abi.encodePacked(proofElement, computedHash));
+            }
+        }
+
+        // Check if the computed hash (root) is equal to the provided root
+        return computedHash;
+    }
+
+    
+    constructor(MyToken token) public payable{
         owner = msg.sender;
+        tokenContract = token;
     }
     
+    /*used to publish the merkle root of a particular years
+      Can only be called by the owner of the contract*/
     function publish(uint year, bytes32 root) public onlyOwner{
-        certificates[year] = root;
+        if(certificates[year] == 0x0)
+            certificates[year] = root;
     }
     
     function getHash(uint year) view public returns (bytes32){
@@ -29,7 +61,15 @@ contract Certificates{
             return false;
     }
     
-    function verify(uint year) public returns (bool){         //TODO : Add merkle path in the parameter and then call oraclize
-                                        //       here to get encrypted hash and then create merkle root 
-    }                                   //       verify. Also get tokens as a fee
+    function verify(bytes32[] memory proof, bytes32 hash, uint year) public payable returns (bytes32){
+        require(tokenContract.balanceOf(msg.sender) >= verificationValue * 10 ** uint256(decimals));
+        bytes32 root = certificates[year];
+        
+        bytes32 isValid = verifyProof(proof, root, hash);
+        
+        //Accept the fee for verifying the certificate
+        tokenContract.acceptFee(msg.sender, verificationValue * 10 ** uint256(decimals));
+        
+        return isValid;
+    }
 }
