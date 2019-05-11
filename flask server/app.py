e5c2ca5f3377217c407197c6eafbe6164cd744df
Yogesh Mangnaik <yogeshmangnaik2012@gmail.com>
2019-05-11 21:41:07 +0530

    Checking for correct smart contract in query endpoint added

--- flask server/app.py
+++ flask server/app.py
@@ -109,9 +109,10 @@ def index():
 def hash():
     normalhash = request.args.get('hash')
     checkhash = Web3.toHex(Web3.soliditySha3(['string'], [normalhash]))
-    print("Checking")
     isValid = (smartcontracts.verifyContract.functions.validRequests(checkhash).call())
-    print("Status : " + str(isValid))
+    if not valid:
+        print("The query request did not come from our smart contract")
+        abort(403)
     privateKey = "0x0bc9b5bf5d3a57829de9c2cc9d82ff3a21b0c6be4f33d9ac19a1807a6f8ef189"
     x = Web3.toHex(Web3.soliditySha3(['bytes32', 'bytes32'], [normalhash, privateKey]))
     data = {}
